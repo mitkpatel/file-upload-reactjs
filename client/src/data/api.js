ee85758c4cd9ba39f38ae0f1a9ec220f52bd1262
Mit Patel <mitkpatel09@gmail.com>
2023-03-31 21:22:04 +0000

    Update api.js

--- client/src/data/api.js
+++ client/src/data/api.js
@@ -1,35 +1,35 @@
-import axios from 'axios';
+import axios from "axios";
 
-const apiUrl = 'https://qd12wj-8080.csb.app/api/';
+const apiUrl = "https://qd12wj-8080.csb.app/api/";
 
 export const singleFileUpload = async (data, options) => {
-    try {
-        await axios.post(apiUrl + 'singleFile', data, options);
-    } catch (error) {
-        throw error;
-    }
-}
+  try {
+    await axios.post(apiUrl + "singleFile", data, options);
+  } catch (error) {
+    throw error;
+  }
+};
 export const getSingleFiles = async () => {
-    try {
-            const {data} = await axios.get(apiUrl + 'getSingleFiles');
-            return data;
-    } catch (error) {
-        throw error;
-    }
-}
+  try {
+    const { data } = await axios.get(apiUrl + "getSingleFiles");
+    return data;
+  } catch (error) {
+    throw error;
+  }
+};
 
 export const multipleFilesUpload = async (data, options) => {
-    try {
-        await axios.post(apiUrl + 'multipleFiles', data, options);
-    } catch (error) {
-        throw error;
-    }
-}
+  try {
+    await axios.post(apiUrl + "multipleFiles", data, options);
+  } catch (error) {
+    throw error;
+  }
+};
 export const getMultipleFiles = async () => {
-    try{
-        const {data} = await axios.get(apiUrl + 'getMultipleFiles');
-        return data;
-    }catch(error){
-        throw error;
-    }
-}
+  try {
+    const { data } = await axios.get(apiUrl + "getMultipleFiles");
+    return data;
+  } catch (error) {
+    throw error;
+  }
+};
